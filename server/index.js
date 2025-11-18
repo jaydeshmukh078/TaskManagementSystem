0671da9a0f820dc639e4cb3ca849e0e7f7fbfbe7
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-11-18 23:25:27 +0530

    a

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use("/admin", AdminRoute);
 app.use("/employee", EmpRoute);
 
-const Port = process.env.PORT || 8000
+const Port = process.env.PORT || 8000 
 app.listen(Port, ()=>{
     console.log(`Server run on ${Port}  Port!`);
 })
